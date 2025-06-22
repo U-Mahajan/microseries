4c0885620ab59c2a178c7d605a68ddbe3e073a32
U-Mahajan <129054765+U-Mahajan@users.noreply.github.com>
2025-06-22 07:18:19 +0530

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -17,17 +17,6 @@ public class Main {
             System.out.println("umesh");
             String name = "umesh mahajan";
         }
-        System.out.printf("Hello and welcome!");
-            for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-            System.out.println("umesh");
-            String name = "umesh mahajan";
-
-        }
-        System.out.printf("Umesh");
             for (int i = 1; i <= 5; i++) {
 
             // Press Shift+F9 to start debugging your code. We have set one breakpoint
